cab60eae0c5a622d434eb9eb14db490b45d584b1
Andrey Efimov <efiand@ya.ru>
2024-03-31 17:36:43 +0300

    Fix declension method.

--- source/scripts/common/utils.js
+++ source/scripts/common/utils.js
@@ -1,19 +1,22 @@
+/* eslint-disable no-magic-numbers */
+
 export const html = (raw, ...values) => String.raw({ raw }, ...values);
 
 export const getTelLink = (tel) => `tel:+${tel.replace(/\D/g, '')}`;
 
 // Выбор словоформы по значению числа
-const PLURAL_THRESHOLD = 5;
 export const getWordAfterNum = (num, [form1, form2 = form1, form3 = form2]) => {
-	const lastDigit = num % 10;
+	const remainder10 = num % 10;
+	const remainder100 = num % 100;
 
-	if (!num || (num % 100) - lastDigit === 10 || lastDigit >= PLURAL_THRESHOLD) {
-		return form3;
-	}
-
-	if (lastDigit === 1) {
+	if (remainder10 === 1 && remainder100 !== 11) {
 		return form1;
 	}
 
-	return form2;
+	const notInSecondDozen = remainder100 < 10 || remainder100 >= 20;
+	if (remainder10 >= 2 && remainder10 <= 4 && notInSecondDozen) {
+		return form2;
+	}
+
+	return form3;
 };
